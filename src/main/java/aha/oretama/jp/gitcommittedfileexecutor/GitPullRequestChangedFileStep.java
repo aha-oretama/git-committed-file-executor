ad3222d04417b9a493de1cfa5efd9eccbb8bc7e5
aha-oretama <sekine_y_529@msn.com>
2017-03-02 16:34:27 +0900

    Github Access

---
+++ src/main/java/aha/oretama/jp/gitcommittedfileexecutor/GitPullRequestChangedFileStep.java
@@ -0,0 +1,65 @@
+package aha.oretama.jp.gitcommittedfileexecutor;
+
+import com.google.inject.Inject;
+import hudson.Extension;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+
+import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
+import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
+import org.jenkinsci.plugins.workflow.steps.AbstractSynchronousStepExecution;
+import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.util.List;
+
+/**
+ * @author aha-oretama
+ */
+public class GitChangedFileStep extends AbstractStepImpl {
+
+    private final String regex;
+    private final String testTargetRegex;
+
+    @DataBoundConstructor
+    public GitChangedFileStep(String regex, String testTargetRegex) {
+        this.regex = regex;
+        this.testTargetRegex = testTargetRegex;
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends AbstractStepDescriptorImpl {
+
+        public DescriptorImpl() {
+            super(Execution.class);
+        }
+
+        @Override
+        public String getFunctionName() {
+            return "gitChangedFiles";
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Get git changed files";
+        }
+    }
+
+
+    public static final class Execution extends AbstractSynchronousStepExecution<List<String>> {
+
+        @Inject
+        private transient GitChangedFileStep step;
+
+        @StepContextParameter
+        private transient Run<?,?> build;
+
+        @StepContextParameter
+        private transient TaskListener listener;
+
+        @Override
+        protected List<String> run() throws Exception {
+            return GitCommittedFileExecutor.getExpressionsForTestInclusion(build,listener, step.regex, step.testTargetRegex);
+        }
+    }
+}
